6d2e041a3ed40ef14a55c25596f3ac99b629f20d
luisalfredosv <luissarabia80@gmail.com>
2021-10-22 10:18:45 -0400

    [update] Importaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TracksModule } from './tracks/tracks.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env`]
+    }),
+    AuthModule,
+    UsersModule,
+    TracksModule,
+  ],
+  providers: [PrismaModule],
 })
 export class AppModule {}
